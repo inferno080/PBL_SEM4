7cc16bf19f9a74c30bcc53928431b937d1ecd5d7
ashmikagupte <63774546+ashmikagupte@users.noreply.github.com>
2021-02-04 20:09:42 +0530

    Text recognition

---
+++ numberplate.py
@@ -0,0 +1,41 @@
+import cv2
+import imutils as imutils
+import numpy as np
+import easyocr
+import pytesseract
+
+img = cv2.imread("Resources\car.jfif")
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.bilateralFilter(gray, 13, 15, 15)
+canny = cv2.Canny(gray, 30, 200)
+contours = cv2.findContours(canny.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
+contours = sorted(contours,key=cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+
+for c in contours:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+    # if our approximated contour has four points, then
+    # we can assume that we have found our screen
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+mask = np.zeros(gray.shape,np.uint8)
+new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+new_image = cv2.bitwise_and(img,img,mask=mask)
+
+(x, y) = np.where(mask == 255)
+(topx, topy) = (np.min(x), np.min(y))
+(bottomx, bottomy) = (np.max(x), np.max(y))
+Cropped = gray[topx:bottomx+1, topy:bottomy+1]
+
+text = pytesseract.image_to_string(Cropped)
+print("Detected license plate Number is:",text)
+cv2.imshow("Car Image", img)
+cv2.imshow("Image", Cropped)
+cv2.waitKey(0)
